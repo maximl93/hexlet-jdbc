5ebefc317208ccae0e1bdf0583482e0e7c9700d3
Maksim Lukin <maximlm93@gmail.com>
2024-12-09 17:15:04 +0500

    third commit

--- src/main/java/hexlet/code/Application.java
+++ src/main/java/hexlet/code/Application.java
@@ -10,14 +10,34 @@ public class Application {
                 statement.execute(sql);
             }
 
-            String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (Statement statement2 = connection.createStatement()) {
-                statement2.executeUpdate(sql2);
+            String sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql2)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "333333333");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.setString(2, "444444444");
+                preparedStatement.executeUpdate();
             }
 
             String sql3 = "SELECT * FROM users";
             try (Statement statement3 = connection.createStatement();) {
                 ResultSet resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+
+            String sql4 = "DELETE FROM users WHERE username = ?";
+            try (PreparedStatement preparedStatement2 = connection.prepareStatement(sql4)){
+                preparedStatement2.setString(1, "Tommy");
+                preparedStatement2.executeUpdate();
+            }
+
+            try (Statement statement4 = connection.createStatement();) {
+                ResultSet resultSet = statement4.executeQuery(sql3);
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
