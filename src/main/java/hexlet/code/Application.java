2e1e967dd47fbbd3972503c579e8403f21c2ff7a
Maksim Lukin <maximlm93@gmail.com>
2024-12-09 16:31:31 +0500

    second commit

--- src/main/java/hexlet/code/Application.java
+++ src/main/java/hexlet/code/Application.java
@@ -4,27 +4,25 @@ import java.sql.*;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test")) {
+            String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(sql);
+            }
 
-        String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        Statement statement = connection.createStatement();
-        statement.execute(sql);
-        statement.close();
+            String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try (Statement statement2 = connection.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        Statement statement2 = connection.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
-
-        String sql3 = "SELECT * FROM users";
-        Statement statement3 = connection.createStatement();
-        ResultSet resultSet = statement3.executeQuery(sql3);
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            String sql3 = "SELECT * FROM users";
+            try (Statement statement3 = connection.createStatement();) {
+                ResultSet resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
         }
-        statement3.close();
-
-        connection.close();
     }
 }
